7547304127ebe419edc2f949b66ddb6ee0684127
Sasha <sashabuskin12@gmail.com>
2026-01-09 00:19:16 +1300

    cell hover added

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,11 +1,28 @@
+
 interface CellProps {
   src: string;
 }
 
-const Cell: React.FC<CellProps> = ({ src }) => (
-  <div className="cell">
-    <img src={src} alt="artwork" />
-  </div>
-);
+const Cell: React.FC<CellProps> = ({ src }) => {
+
+  const filename = decodeURIComponent(src.split("/").pop() || "");
+
+  const nameWithoutExt = filename.replace(/\.[^/.]+$/, "");
+
+  const [title = "Untitled", dimension = "", medium = ""] = nameWithoutExt.split("_");
+
+  return (
+    <div className="cell">
+      <img src={src} alt={title} />
+      <div className="overlay">
+        <div className="title">{title}</div>
+        <div className="details">
+          <div id="dimension">{dimension}</div>
+          <div id="medium">{medium}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Cell;
