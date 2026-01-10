bcbf8d8b6b23b2e4937567be5d750efff54026b6
Sasha <sashabuskin12@gmail.com>
2026-01-10 13:34:34 +1300

    pre-artworkpage BEFORE THE STORM

---
+++ src/components/HomeInfo.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import sign from "../assets/sign.png";
+
+const HomeInfo = () => {
+  return (
+    <>
+      <img className="sign" src={sign} alt="Sign" />
+      <div className="ArtistName">Robert J. Domino</div>
+      <div className="subCaps">Painter & Illustrator - Caen, Normandy</div>
+      <span className="homeLinks">Paintings</span>
+      <span className="homeLinks">Drawings</span>
+      <span className="homeLinks">Sculptures</span>
+
+      <div className="bottomLinksBox">
+        <div className="bottomLink">Artist,</div>
+        <div className="bottomLink">Journal,</div>
+        <div className="bottomLink">About,</div>
+        <div className="bottomLink">Video,</div>
+        <div className="bottomLink">Archive,</div>
+        <div className="bottomLink">Contact</div>
+      </div>
+      <div className="creditText">
+        Copyright © 2026 R.J. Domino. Site by Sasha Buskin
+      </div>
+    </>
+  );
+};
+
+export default HomeInfo;
